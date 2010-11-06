4f903921bc7d07a7c070db73e0699adb17d717c0
Thomas Eng (Work) <thomas.tinnesand.eng@gmail.com>
2010-11-06 21:22:12 +0100

    date_time needed to be in global namespace to work with icalendar

--- lib/kerio-caldav/date_time.rb
+++ lib/kerio-caldav/date_time.rb
@@ -1,7 +1,5 @@
-module KerioCaldav
-	class DateTime
-		def to_time
-			Time.mktime(self.year, self.month, self.day, self.hour, self.min, self.sec)
-		end
+class DateTime
+	def to_time
+		Time.mktime(self.year, self.month, self.day, self.hour, self.min, self.sec)
 	end
 end
